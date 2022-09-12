f21fba2e20edd8ab44e900c5d9c43cce59dae146
josefin-ferrada <jferrada@eclass.cl>
2022-09-11 23:12:36 -0300

    libreria pixel

--- src/Components/Formulario/Formulario.tsx
+++ src/Components/Formulario/Formulario.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactPixel from 'react-facebook-pixel';
 // import Axios from "axios";
 import {
   FormControl,
@@ -9,6 +10,12 @@ import {
 } from "@chakra-ui/react";
 
 export const Formulario = ({ end }) => {
+
+
+ReactPixel.init('1245470039549197');
+
+
+
   const [phone, setPhone] = React.useState(0)
   const [name, setName] = React.useState('')
 
@@ -16,7 +23,8 @@ export const Formulario = ({ end }) => {
   const apiKey = 'KuZ2ThsRXHedjuXcsnCTFfnQGDan6AWtWJvDX4eMRmUPHc3gFP66WkRnj52Zbsk74DYsNRKK9CqtQYbeCjDbh4YhEUzS8GThcv2BjmZTBgjG9jJB98X6AjfrnyhPy44SXZDbkAQwHreWE6vctawvmhxB7YuwT2gCP2aj42WMufcjDPQzEND9AMdJGeSprN5yck6wXB6XB27xRs52WcrQpbqBUYBVQenQuGy5JsTVeqE4ZW92kxKjXT5mGbQXEzwV'
     const fetchData = () => {
       
-      fetch(`https://api-blacksteel.clokpi.com/api/blacksteel/services/lead/rs?order_id=000000000001&phone=${phone}&name=${name}&api_key=${apiKey}&good_id=34300&canal=prueba&anunciante=miguel`, { mode: "no-cors"} )
+      fetch(`http://api-blacksteel.clokpi.com/api/blacksteel/services/lead/rs?order_id=000000000002&phone=${phone}&name=${name}&api_key=${apiKey}&good_id=34300&canal=prueba&anunciante=facebook`, {
+        method: "POST" })
       .then(res => {
         console.log(res)
       })
@@ -29,6 +37,10 @@ export const Formulario = ({ end }) => {
     const handlePhoneChange = (e) => setPhone(e.target.value)
 
     const handleSubmit = () => {
+      ReactPixel.trackSingle('1245470039549197', 'Purchase', {
+        value: 29990,
+        currency: 'CLP',
+      }); // For tracking default events.
       fetchData()
     }
 
