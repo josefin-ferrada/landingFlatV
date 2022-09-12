700110b2db1533fa37f40e259f3f8fd2b344540b
josefin-ferrada <jferrada@eclass.cl>
2022-09-11 23:14:17 -0300

    libreria pixel

--- src/Components/Formulario/Formulario.tsx
+++ src/Components/Formulario/Formulario.tsx
@@ -23,7 +23,7 @@ ReactPixel.init('1245470039549197');
   const apiKey = 'KuZ2ThsRXHedjuXcsnCTFfnQGDan6AWtWJvDX4eMRmUPHc3gFP66WkRnj52Zbsk74DYsNRKK9CqtQYbeCjDbh4YhEUzS8GThcv2BjmZTBgjG9jJB98X6AjfrnyhPy44SXZDbkAQwHreWE6vctawvmhxB7YuwT2gCP2aj42WMufcjDPQzEND9AMdJGeSprN5yck6wXB6XB27xRs52WcrQpbqBUYBVQenQuGy5JsTVeqE4ZW92kxKjXT5mGbQXEzwV'
     const fetchData = () => {
       
-      fetch(`http://api-blacksteel.clokpi.com/api/blacksteel/services/lead/rs?order_id=000000000002&phone=${phone}&name=${name}&api_key=${apiKey}&good_id=34300&canal=prueba&anunciante=facebook`, {
+      fetch(`http://api-blacksteel.clokpi.com/api/blacksteel/services/lead/rs?order_id=000000000002&phone=${phone}&name=${name}&api_key=${apiKey}&good_id=34300&canal=landingReact&anunciante=trifuerza`, {
         method: "POST" })
       .then(res => {
         console.log(res)
